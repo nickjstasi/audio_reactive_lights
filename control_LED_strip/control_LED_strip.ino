572ac88abe5c9cf1ea7fc3f2ec4e62010fcc390b
Nicholas Stasi <njstasi@gmail.com>
2019-11-24 23:00:35 -0600

    adding gitignore and cleaning up code

--- control_LED_strip/control_LED_strip.ino
+++ control_LED_strip/control_LED_strip.ino
@@ -13,7 +13,9 @@
 #define RESET_PIN 12
 
 int modes[NUM_MODES] = {};
-CRGB leds[NUM_LEDS];
+CRGB ledsBass[NUM_LEDS];
+CRGB ledsMids[NUM_LEDS];
+CRGB ledsHighs[NUM_LEDS];
 int colors[NUM_LEDS];
 int msg_level[7];
 int msgOutPin = A5;
@@ -21,7 +23,7 @@ int msgStrobePin = STROBE_PIN;
 int msgResetPin = RESET_PIN;
 int centerLed = NUM_LEDS / 2;
 int buttonPin = 2;
-int buttonState = 0;
+int buttonState = 1;
 
 void setup() {
   delay(3000);
@@ -30,9 +32,9 @@ void setup() {
   attachInterrupt(0, buttonPressed, FALLING); //interrupt for when button is pressed
   //Serial.begin(9600); //for debug
   ///////////////////SETUP FAST LED///////////////////////////////
-  FastLED.addLeds<WS2812, LED_PIN_BASS, GRB>(leds, NUM_LEDS);
-  //FastLED.addLeds<WS2812, LED_PIN_MIDS, GRB>(leds, NUM_LEDS);
-  //FastLED.addLeds<WS2812, LED_PIN_HIGHS, GRB>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2812, LED_PIN_BASS, GRB>(ledsBass, NUM_LEDS);
+  FastLED.addLeds<WS2812, LED_PIN_MIDS, GRB>(ledsMids, NUM_LEDS);
+  FastLED.addLeds<WS2812, LED_PIN_HIGHS, GRB>(leds, NUM_LEDS);
   LEDS.setBrightness(255);
   //////////////////////////////////////////////////////////////////
 
@@ -61,97 +63,33 @@ void loop() {
    //Check the analog input
    //int loudness = analogRead(A0);
    //int highest_led = map(loudness, 0, 388, 0, NUM_LEDS);
-   int level[7];
+   int level[7]; //array to hold values from MSGEQ7
    
   // Cycle through each frequency band by pulsing the strobe.
   for (int i = 0; i < 7; i++) {
     digitalWrite       (msgStrobePin, LOW);
     delayMicroseconds  (40);                  // Delay necessary due to timing diagram
-    level[i] =         analogRead (msgOutPin);
+    level[i] =         analogRead (msgOutPin); //read the level
     digitalWrite       (msgStrobePin, HIGH);
     delayMicroseconds  (40);                    // Delay necessary due to timing diagram  
   }
 
-  int loudDistance = map(level[0], 0, 1023, 0, NUM_LEDS/2); //get the distance from the center that the lights should flash based on loudness of level[2]
-  //Serial.print("level2: "); Serial.print(level[2]); Serial.print(" loudDistance: ");  Serial.print(loudDistance); Serial.println("");
-
-  //set the leds to light up
-  int lowerBound = centerLed - loudDistance;
-  int upperBound = centerLed + loudDistance;
-  for(int i = lowerBound; i< upperBound; i++){
-    leds[i] = CRGB::Green;
+//TODO get rid of magic numbers
+  switch(buttonState){
+    case 1: 
+        midBounce(level);
+        break;
+    case 2:
+        peakBounce(level);
+        break;
+    case 3:
+        allWhite(level);
+        break;
+    default:
+        midBounce(level);
   }
-  //set lights outside this range to be black
-  for(int i = 0; i < lowerBound; i++)
-  {
-    leds[i] = CRGB::Black;
-  }
-  for(int i = upperBound; i < NUM_LEDS; i++){
-    leds[i] = CRGB::Black;
-  }
-  FastLED.show();
-  delay(25);//wait a lil
-  
-//  for (int i = 0; i < 7; i++) {
-//    Serial.print       (level[i]);
-//    Serial.print       ("   ");
-//  }
-//  Serial.println ();  
-   
 
-
-
-
-
-
-
-   
-//   //highest_led = highest_led - 25;
-//   //Serial.print("loudness: "); Serial.println(loudness); //this was messing with the pwm output
-//   //Serial.print("highest_led: "); Serial.println(highest_led);  //seems to be working properly up to here
-//   
-//   //light up all led's up to the highest_led
-//   int i;
-//   for(i = 1; i < highest_led; i++){
-//      if(i < (NUM_LEDS / 3)){
-//        leds[i] = CRGB::Green;
-//      }
-//      else if(i < (2*NUM_LEDS / 3)){
-//        leds[i] = CRGB::Yellow;
-//      }
-//      else {
-//        leds[i] = CRGB::Red;
-//      }
-//   }
-//   
-//   //turn off leds higher than highest_led
-//   int j = highest_led;
-//   if(j == 0) j = 1;
-//   for( ; j < NUM_LEDS; j++){
-//      leds[j] = CRGB::Black;
-//   }
-//
-//   FastLED.show();
-//   delay(5); //wait a lil bit
-
-
-
-   
-   // Move a single white led 
-//   for(int whiteLed = 0; whiteLed < NUM_LEDS; whiteLed = whiteLed + 1) {
-//      // Turn our current led on to white, then show the leds
-//      leds[whiteLed] = CRGB::White;
-//
-//      // Show the leds (only one of which is set to white, from above)
-//      FastLED.show();
-//
-//      // Wait a little bit
-//      delay(10);
-//
-//      // Turn our current led back to black for the next loop around
-//      //leds[whiteLed] = CRGB::Black;
-//   }
-  }//loop
+}//loop
 
 /**
  * Helper function used to get the user selected mode
@@ -164,14 +102,83 @@ int checkMode(){
 int clearStrip(){
   int i = 0;
   for(i = 0; i < NUM_LEDS; i++){
-    leds[i] = CRGB::Black;
+    ledsBass[i] = CRGB::Black;
+    ledsMids[i] = CRGB::Black;
+    ledsHighs[i] = CRGB::Black;
   }
   FastLED.show();
 }
 
 int buttonPressed(){
-    buttonState++;
+    buttonState++; //increment state
+    if(buttonState == NUM_MODES){ //roll over
+      buttonState = 0;
+    }
     //Serial.print(buttonState);
     //Serial.println("");
   
 }//buttonPressed
+
+void midBounce(int* level){
+  int loudDistance = map(level[0], 0, 1023, 0, NUM_LEDS/2); //get the distance from the center that the lights should flash based on loudness of level[2]
+  
+  //set the leds to light up
+  int lowerBound = centerLed - loudDistance;
+  int upperBound = centerLed + loudDistance;
+  for(int i = lowerBound; i< upperBound; i++){
+    ledsBass[i] = CRGB::Blue;
+    ledsMids[i] = CRGB::Green;
+  }
+  //set lights outside this range to be black
+  for(int i = 0; i < lowerBound; i++)
+  {
+    ledsBass[i] = CRGB::Black;
+    ledsMids[i] = CRGB::Black;
+  }
+  for(int i = upperBound; i < NUM_LEDS; i++){
+    ledsBass[i] = CRGB::Black;
+    ledsMids[i] = CRGB::Black;
+  }
+  FastLED.show();
+  delay(25);//wait a lil
+}//midBounce
+
+void peakBounce(int* level){
+   //TODO: decide how highest_led should be calculated
+   int highest_led = 0;
+   //light up all led's up to the highest_led
+   int i;
+   for(i = 1; i < highest_led; i++){
+      if(i < (NUM_LEDS / 3)){
+        leds[i] = CRGB::Green;
+      }
+      else if(i < (2*NUM_LEDS / 3)){
+        leds[i] = CRGB::Yellow;
+      }
+      else {
+        leds[i] = CRGB::Red;
+      }
+   }
+   
+   //turn off leds higher than highest_led
+   int j = highest_led;
+   if(j == 0) j = 1;
+   for( ; j < NUM_LEDS; j++){
+      leds[j] = CRGB::Black;
+   }
+
+   FastLED.show();
+   delay(25); //wait a lil bit
+
+}//peakBounce
+
+void allWhite(){
+  for(int i = 0; i < NUM_LEDS; i++){
+    ledsBass[i] = CRGB::White;
+    ledsMids[i] = CRGB::White;
+    ledsHighs[i] = CRGB::White;
+  }
+  FastLED.show()
+  delay(25);
+
+}
